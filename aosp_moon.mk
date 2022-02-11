96072cb17c927ac01adb1265f0aaf3dd6f17e607
Sarthak Roy <sarthakroy2002@gmail.com>
2022-02-11 12:26:32 +0000

    moon: Update Build fp from Coral Feb'22 Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com> Change-Id: I881a86e50b2aff119268dfcd7cfbcff8bc21d9b7

--- aosp_moon.mk
+++ aosp_moon.mk
@@ -31,7 +31,7 @@ PRODUCT_MANUFACTURER := realme
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Build info
-BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:12/SQ1A.220205.002/8010174:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=moon \
     PRODUCT_NAME=moon \
